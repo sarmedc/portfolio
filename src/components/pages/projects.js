8cfe757db8574d0f66ce0b3506ab8c1e5d006a7e
Sarmed Chaudhry <chaudhrysarmed@gmail.com>
2017-11-13 00:14:57 -0800

    updated project

--- src/components/pages/projects.js
+++ src/components/pages/projects.js
@@ -13,34 +13,31 @@ class Projects extends Component {
       		<Grid>
 			    <Row className="show-grid">
 			      <Col sm={6} md={4}><img className="projectImg" 			      
-			      	src={require('../../Assets/img/reddit-clone.png')} alt=""/><br/>
+			      	src={require('../../Assets/img/snh.png')} alt=""/><br/>
 			      	<div className="pDiv">			      	
-			      	<h2 className="pTitle">Reddit Clone</h2>
-      				<p className="pDescription">Developed a lite clone of Reddit.com that displays the top current posts of a given subreddit</p>
-      				</div>
-      				<p>      		
-                    	<Button className="btn" bsStyle="link" bsSize="large"><a href="https://github.com/sarmedc/Reddit-Clone" className="link">More Info</a></Button>
-                	</p>
+			      	<h2 className="pTitle">Social News Hub</h2>
+      				<p className="pDescription">SNH displays the top posts of popular social news and tech sites all in one page</p>
+      				</div>      				
+                    <Button className="btn" bsStyle="link" bsSize="large"><a href="https://github.com/sarmedc/Social-News-Hub" className="link">More Info</a></Button>
+                	
 			      </Col>
 			      <Col sm={6} md={4}><img className="projectImg" 
 			      	src={require('../../Assets/img/wiki-search.png')} alt=""/><br/>
 			      	<div className="pDiv">
 			      	<h2 className="pTitle">Wiki Search</h2>
 			      	<p className="pDescription">Wiki Search displays to users all related search results that links to their respective articles</p>
-			      	</div>
-			      	<p>
-                    	<Button className="btn" bsStyle="link" bsSize="large"><a href="https://github.com/sarmedc/Wiki-Search" className="link">More Info</a></Button>
-                	</p>
+			      	</div>			      
+                    <Button className="btn" bsStyle="link" bsSize="large"><a href="https://github.com/sarmedc/Wiki-Search" className="link">More Info</a></Button>
+                	
 			      </Col>
 			      <Col sm={6} md={4}><img className="projectImg" 
 			      	src={require('../../Assets/img/clean-app.png')} alt=""/><br/>
 			      	<div className="pDiv">
 			      	<h2 className="pTitle">Clean With Friends</h2>
-			      	<p className="pDescription">A web application that encourages users to collaborate with their housemates to keep their living space clean. Built using Node.js, Express, and MongoDB, as well as hosted on Heroku.</p>
+			      	<p className="pDescription">A web application that encourages users to collaborate with their housemates to keep their living space clean.</p>
 			      	</div>
-			      	<p>
-                    	<Button className="btn" bsStyle="link" bsSize="large"><a href="https://github.com/sarmedc/CleaningApp" className="link">More Info</a></Button>
-                	</p>
+                    <Button className="btn" bsStyle="link" bsSize="large"><a href="https://github.com/sarmedc/CleaningApp" className="link">More Info</a></Button>
+                	
 			      </Col>			      
 			    </Row>
 			    <br/>
